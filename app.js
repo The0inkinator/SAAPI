9ac366c376a3031ccb59516adf316f006010fba5
TheOinkinator <theoinkinator@gmail.com>
2023-09-13 20:33:51 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+const data = {
+  message: "hello, API",
+  timestamp: new Date(),
+};
+
+app.get("/api/data", (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`API server is running on port ${port}`);
+});
