31eb01ad680befac614cec1c3e9c5b038648b9c1
TheOinkinator <theoinkinator@gmail.com>
2023-09-14 09:54:10 -0700

    add get index reading

--- index.js
+++ index.js
@@ -11,14 +11,35 @@ const dataFolderPath = path.join(__dirname, "data");
 
 app.get("/api/data/:path(*)", (req, res) => {
   const { path: filePath } = req.params;
-  const requestedFilePath = path.join(dataFolderPath, filePath + ".json");
+  const requestedFilePath = path.join(dataFolderPath, filePath);
 
-  fs.access(requestedFilePath, fs.constants.F_OK, (err) => {
+  // Check if the requested path points to a directory
+  if (
+    fs.existsSync(requestedFilePath) &&
+    fs.statSync(requestedFilePath).isDirectory()
+  ) {
+    // If it's a directory, try serving an "index.json" file from within the directory
+    const indexPath = path.join(requestedFilePath, "index.json");
+    if (fs.existsSync(indexPath)) {
+      return fs.readFile(indexPath, "utf8", (err, data) => {
+        if (err) {
+          return res.status(500).json({ error: "Internal server error" });
+        }
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      });
+    }
+  }
+
+  // If it's not a directory or no "index.json" is found, try serving the path with ".json" extension
+  const jsonFilePath = requestedFilePath + ".json";
+
+  fs.access(jsonFilePath, fs.constants.F_OK, (err) => {
     if (err) {
       return res.status(404).json({ error: "Data not found" });
     }
 
-    fs.readFile(requestedFilePath, "utf8", (err, data) => {
+    fs.readFile(jsonFilePath, "utf8", (err, data) => {
       if (err) {
         return res.status(500).json({ error: "Internal server error" });
       }
