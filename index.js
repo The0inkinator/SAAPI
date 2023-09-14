0830f611ef04bb8e8c402f534117b02058749e5c
TheOinkinator <theoinkinator@gmail.com>
2023-09-13 23:17:42 -0700

    latest version of get functionality

--- index.js
+++ index.js
@@ -9,25 +9,35 @@ app.use(cors());
 
 const dataFolderPath = path.join(__dirname, "data");
 
-app.get("/api/data/*", (req, res) => {
+app.get("/api/data/*/:filename?", (req, res) => {
   const urlSegments = req.url.split("/").filter(Boolean);
   const folderPath = path.join(dataFolderPath, ...urlSegments);
+  const { filename } = req.params;
+  const filePath = path.join(folderPath, `${filename}.json`);
   const indexPath = path.join(folderPath, "index.json");
 
-  // Check if the requested folder exists
-  fs.access(folderPath, fs.constants.F_OK, (err) => {
+  // Check if the requested file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
-      return res.status(404).json({ error: "Folder not found" });
-    }
+      // Check if the index.json file exists in the folder
+      fs.access(indexPath, fs.constants.F_OK, (err) => {
+        if (err) {
+          return res.status(404).json({ error: "Data not found" });
+        }
 
-    // Check if the index.json file exists in the folder
-    fs.access(indexPath, fs.constants.F_OK, (err) => {
-      if (err) {
-        return res.status(404).json({ error: "Data not found" });
-      }
+        // Read and send the JSON data from the index.json file
+        fs.readFile(indexPath, "utf8", (err, data) => {
+          if (err) {
+            return res.status(500).json({ error: "Internal server error" });
+          }
 
-      // Read and send the JSON data from the index.json file
-      fs.readFile(indexPath, "utf8", (err, data) => {
+          const jsonData = JSON.parse(data);
+          res.json(jsonData);
+        });
+      });
+    } else {
+      // Read and send the specified JSON data
+      fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
           return res.status(500).json({ error: "Internal server error" });
         }
@@ -35,7 +45,7 @@ app.get("/api/data/*", (req, res) => {
         const jsonData = JSON.parse(data);
         res.json(jsonData);
       });
-    });
+    }
   });
 });
 
