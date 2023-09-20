390b371a2c7ee0f21c6ab48f519b7c232b111b1a
TheOinkinator <theoinkinator@gmail.com>
2023-09-20 13:09:53 -0700

    add connection w/out connecting

--- index.js
+++ index.js
@@ -3,47 +3,24 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const { Client } = require("pg");
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 
-const dataFolderPath = path.join(__dirname, "data");
+// Use the Heroku-provided DATABASE_URL
+const databaseUrl = process.env.DATABASE_URL; // This will be automatically set by Heroku
+
+// Create a PostgreSQL client
+const client = new Client({
+  connectionString: databaseUrl,
+  ssl: { rejectUnauthorized: false }, // Add this line for Heroku PostgreSQL
+});
+
+client.connect(); // Connect to the database
 
 app.get("/api/data/", (req, res) => {
   res.send("hello");
-  // const { path: filePath } = req.params;
-  // const requestedFilePath = path.join(dataFolderPath, filePath);
-  // // Check if the requested path points to a directory
-  // if (
-  //   fs.existsSync(requestedFilePath) &&
-  //   fs.statSync(requestedFilePath).isDirectory()
-  // ) {
-  //   // If it's a directory, try serving an "index.json" file from within the directory
-  //   const indexPath = path.join(requestedFilePath, "index.json");
-  //   if (fs.existsSync(indexPath)) {
-  //     return fs.readFile(indexPath, "utf8", (err, data) => {
-  //       if (err) {
-  //         return res.status(500).json({ error: "Internal server error" });
-  //       }
-  //       const jsonData = JSON.parse(data);
-  //       res.json(jsonData);
-  //     });
-  //   }
-  // }
-  // // If it's not a directory or no "index.json" is found, try serving the path with ".json" extension
-  // const jsonFilePath = requestedFilePath + ".json";
-  // fs.access(jsonFilePath, fs.constants.F_OK, (err) => {
-  //   if (err) {
-  //     return res.status(404).json({ error: "Data not found" });
-  //   }
-  //   fs.readFile(jsonFilePath, "utf8", (err, data) => {
-  //     if (err) {
-  //       return res.status(500).json({ error: "Internal server error" });
-  //     }
-  //     const jsonData = JSON.parse(data);
-  //     res.json(jsonData);
-  //   });
-  // });
 });
 
 app.listen(port, () => {
