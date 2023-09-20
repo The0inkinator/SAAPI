36bce498c9b7fb85b1cd8f9f3722d58d5a153113
TheOinkinator <theoinkinator@gmail.com>
2023-09-20 13:39:22 -0700

    hard code connection string

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const databaseUrl = process.env.DATABASE_URL; // This will be automatically set 
 
 // Create a PostgreSQL client
 const client = new Client({
-  connectionString: databaseUrl,
+  connectionString:
+    "postgres://wenzitrhfbdowm:71f569a7498666093ed5fded6345fc7f3af47c761726d98c888f0e9558817433@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d2acbcolfb7qh7",
   ssl: { rejectUnauthorized: false }, // Add this line for Heroku PostgreSQL
 });
 
