c7ec2a86948719dfcbe48cbdcdc73ae1404789e7
TheOinkinator <theoinkinator@gmail.com>
2023-09-25 21:26:29 -0700

    update connection 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { Client } = require("pg");
 const app = express();
 const port = process.env.PORT || 3001;
-//update connection 2
+//update connection 3
 
 app.use(cors());
 
